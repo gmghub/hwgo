09d9ea8466c3c1c4256fb687c5701fc9a6d7c222
gm <gmglist@yandex.ru>
2022-05-21 20:35:39 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,50 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	normalizedMap := make(map[string]int, len(s)/2)
+	for _, v := range strings.Fields(s) {
+		vn := strings.ToLower(v)
+		vn = strings.Trim(vn, ",.!?()'")
+		if vn == "-" {
+			continue
+		}
+		normalizedMap[vn] += 1
+	}
+
+	strFreqSlice := make([]struct {
+		str  string
+		freq int
+	}, len(normalizedMap))
+	i := 0
+	for k, v := range normalizedMap {
+		strFreqSlice[i].str = k
+		strFreqSlice[i].freq = v
+		i++
+	}
+
+	sort.Slice(strFreqSlice, func(i, j int) bool {
+		if strFreqSlice[i].freq > strFreqSlice[j].freq {
+			return true
+		} else if strFreqSlice[i].freq < strFreqSlice[j].freq {
+			return false
+		}
+		if strings.Compare(strFreqSlice[i].str, strFreqSlice[j].str) < 0 {
+			return true
+		}
+		return false
+	})
+
+	res := []string{}
+	for i := 0; i < 10; i++ {
+		if i > len(strFreqSlice)-1 {
+			break
+		}
+		res = append(res, strFreqSlice[i].str)
+	}
+	return res
 }
