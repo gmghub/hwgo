7f23feaf7a6383d58d77d32a7751c7dd5b5e80b1
gm <gmglist@yandex.ru>
2022-07-06 13:40:20 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,89 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrUnsupportedFile        = errors.New("unsupported file")
+	ErrOffsetExceedsFileSize  = errors.New("offset exceeds file size")
+	ErrInputFileNotSpecified  = errors.New("input file not specified")
+	ErrOutputFileNotSpecified = errors.New("output file not specified")
+	ErrPartiallyCopied        = errors.New("file partially copied")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var err error
+	if fromPath == "" {
+		return ErrInputFileNotSpecified
+	}
+	if toPath == "" {
+		return ErrOutputFileNotSpecified
+	}
+
+	inFileInfo, err := os.Stat(fromPath)
+	if err != nil || !inFileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset > inFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	outFileInfo, err := os.Stat(toPath)
+	if err == nil && !outFileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	inFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	var (
+		totalToWrite int64
+		totalWritten int64
+		blockSize    int64
+	)
+
+	blockSize = 4096
+	totalToWrite = inFileInfo.Size()
+	if offset != 0 {
+		totalToWrite -= offset
+		if _, err = inFile.Seek(offset, 0); err != nil {
+			return err
+		}
+	}
+	if limit != 0 && limit < totalToWrite {
+		totalToWrite = limit
+	}
+
+	bar := pb.Full.Start64(totalToWrite)
+	reader := io.LimitReader(inFile, totalToWrite)
+	barReader := bar.NewProxyReader(reader)
+
+	for {
+		written, err := io.CopyN(outFile, barReader, blockSize)
+		totalWritten += written
+		if errors.Is(err, io.EOF) {
+			break
+		}
+	}
+
+	bar.Finish()
+
+	if totalWritten != totalToWrite {
+		return ErrPartiallyCopied
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,52 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	var (
+		in     = "testdata/input.txt"
+		out    = "out.txt"
+		offset int64
+		limit  int64
+	)
+
+	t.Run("no input filename", func(t *testing.T) {
+		err := Copy("", out, offset, limit)
+		require.ErrorIs(t, err, ErrInputFileNotSpecified)
+	})
+
+	t.Run("no output filename", func(t *testing.T) {
+		err := Copy(in, "", offset, limit)
+		require.ErrorIs(t, err, ErrOutputFileNotSpecified)
+	})
+
+	t.Run("unsupported input file", func(t *testing.T) {
+		err := Copy("/dev/urandom", out, offset, limit)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("unsupported output file", func(t *testing.T) {
+		err := Copy(in, "/dev/null", offset, limit)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		info, _ := os.Stat(in)
+		err := Copy(in, out, info.Size()+1, limit)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("limit exceeds file size", func(t *testing.T) {
+		info, _ := os.Stat(in)
+		fsize := info.Size()
+		err := Copy(in, out, offset, fsize+10)
+		require.Nil(t, err)
+		info, _ = os.Stat(out)
+		require.Equal(t, fsize, info.Size())
+	})
 }
